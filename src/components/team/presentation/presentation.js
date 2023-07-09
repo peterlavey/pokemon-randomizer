e4c676fe077614e23367881b79f916246f2dff49
Pedro Cisternas <pedrocisternasd@gmail.com>
2023-07-09 17:14:25 -0400

    feat: show members in scale

--- src/components/team/presentation/presentation.js
+++ src/components/team/presentation/presentation.js
@@ -1,13 +1,29 @@
 import React, {useEffect, useState} from "react";
 import './presentation.styles.scss';
 
-// con el tanh no queda a escala pero se ve bien
-// poner al mas grande detras del mas pequeño, en orden asc sería ([6 <- 1], [5 <- 2], [4 <- 3])
 
 export const Presentation = ({team}) => {
     const [members, setMembers] = useState([]);
-    const [scale, setScale] = useState(151);
+    const [maxHeight, setMaxHeight] = useState();
     const byHeight = (a,b) => a.height - b.height;
+
+    const adjustHeight = pokemons => {
+        const ekans = {id: 23, rolls: 3};
+        const arbok = {id: 24, rolls: 3};
+        const onix = {id: 95, rolls: 2};
+        const gyarados = {id: 130, rolls: 2};
+        const dratini = {id: 147, rolls: 2};
+        const dragonair = {id: 148, rolls: 2};
+        const dewgong = {id: 87, rolls: 1.5};
+        const exceptions = [ekans, arbok, onix, gyarados, dratini, dragonair, dewgong];
+        return pokemons.map((pokemon) => {
+            const exception = exceptions.find((exception) => exception.id === pokemon.id);
+            if (exception) {
+                return {...pokemon, height: pokemon.height / exception.rolls}
+            }
+            return pokemon
+        });
+    }
     const prepareMembers = () => {
         const _members = [];
         const ordered = team.sort(byHeight);
@@ -17,24 +33,31 @@ export const Presentation = ({team}) => {
         _members.push(ordered[1]);
         _members.push(ordered[3]);
         _members.push(ordered[0]);
-        setMembers(_members);
+        setMembers(adjustHeight(_members));
     };
+    const getScaleHeight = (pokemon) => {
+        const scale = 200;
+        return (pokemon.height / maxHeight) * scale;
+    }
     useEffect(() => {
         if (team.length) {
             prepareMembers();
         }
     }, [team]);
+    useEffect(() => {
+        setMaxHeight(Math.max(...members.map(({height}) => height)));
+    }, [members]);
 
     return (
         <div className='presentation'>
             {
                 members.map((pokemon, index) => {
-                    const zIndex = parseInt(Math.max(...members.map(({height}) => height)) - pokemon.height) * 10;
-                    console.log(`pokemon: ${pokemon.name.english} zIndex: ${zIndex}`)
+                    const zIndex = Math.round(maxHeight - pokemon.height) * 10;
+                    console.log(`pokemon: ${pokemon.name.english} height: ${pokemon.height}`)
                     return (
                         <img
                             src={`images/${pokemon.id}.png`}
-                            width={Math.tanh(pokemon.height) * scale}
+                            width={getScaleHeight(pokemon)}
                             className={`member${index}`}
                             style={{zIndex}}
                             key={pokemon.id}
