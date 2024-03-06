0af16c4c3e9e3812af0869dc1f7d888baf68dff3
Pedro Cisternas <pedrocisternasd@gmail.com>
2024-03-06 17:55:29 -0300

    feat: sound effects added to pokeballs

--- src/components/team/choosePokeballs/choosePokeballs.js
+++ src/components/team/choosePokeballs/choosePokeballs.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {POKEBALL} from "../pokeballs/pokeball/pokeball";
 import "./choosePokeballs.styles.scss";
 
 
-export const ChoosePokeballs = ({pokeballs, setPokeballs}) => (
-    <div className='choosePokeballs'>
-        {
-            Object.values(POKEBALL).map(pokeball => (
-                <img
-                    onClick={() => setPokeballs([...pokeballs, pokeball])}
-                    src={pokeball.img}
-                    key={pokeball.name}
-                    alt={pokeball.name}
-                />
-            ))
-        }
-    </div>
-);
+export const ChoosePokeballs = ({pokeballs, setPokeballs}) => {
+    const [sfxSelect] = useState(new Audio('https://cdn.jsdelivr.net/gh/peterlavey/pokemon-content/sfx/pokeball_tick.mp3'));
+    const select = (pokeball) => {
+        setPokeballs([...pokeballs, pokeball]);
+        sfxSelect.play();
+    }
+
+    return (
+        <div className='choosePokeballs'>
+            {
+                Object.values(POKEBALL).map(pokeball => (
+                    <img
+                        onClick={() => select(pokeball)}
+                        src={pokeball.img}
+                        key={pokeball.name}
+                        alt={pokeball.name}
+                    />
+                ))
+            }
+        </div>
+    )
+};
 
 export default ChoosePokeballs;
