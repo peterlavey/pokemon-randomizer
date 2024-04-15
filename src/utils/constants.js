1c924cb45cb6534d6cc6c266be6f5cfceb7d8ef1
Pedro Cisternas <pedrocisternasd@gmail.com>
2024-04-15 18:29:05 -0400

    feat: open ball sfx fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const SFX_POKEBALL_TICK = 'https://cdn.jsdelivr.net/gh/peterlavey/pokemon-content/sfx/pokeball_tick.mp3';
-export const SFX_POKEBALL_OPEN = 'https://cdn.jsdelivr.net/gh/peterlavey/pokemon-content/sfx/pokeball_open.mp3';
+export const SFX_POKEBALL_OPEN = 'https://cdn.jsdelivr.net/gh/peterlavey/pokemon-content/sfx/pokeball_open30.mp3';
 export const SFX_POKEMON_TEAM = 'https://cdn.jsdelivr.net/gh/peterlavey/pokemon-content/sfx/pokemon_team.mp3';
