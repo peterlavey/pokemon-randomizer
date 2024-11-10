bc20a2782b4b026a641a98cce6d06cd920eacaaf
Pedro Cisternas <pedrocisternasd@gmail.com>
2024-11-10 15:45:11 -0300

    feat: shows more images on carousel

--- src/components/team/choosePokeballs/carousel/carousel.js
+++ src/components/team/choosePokeballs/carousel/carousel.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './carousel.styles.scss';
 import {delay, getByTier} from "../../../../utils/utils";
 import {useSoundContext} from "../../../../contexts/soundContext";
 
 
 export const Carousel = ({pokeball, onSelect}) => {
+    const [pokemonImg, setPokemonImg] = useState([]);
     const {tickSfx} = useSoundContext();
+
+    useEffect(() => {
+        const images = pokeball.tiers.flatMap(getByTier);
+        if (pokeball.name === 'Masterball') {
+            setPokemonImg([...images, ...images, ...images, ...images, ...images, ...images, ...images]);
+        } else {
+            setPokemonImg([...images, ...images, ...images, ...images]);
+        }
+    }, [pokeball.name, pokeball.tiers]);
 
     const handleSelect = async () => {
         tickSfx.currentTime = 0;
@@ -23,7 +33,7 @@ export const Carousel = ({pokeball, onSelect}) => {
                 alt={pokeball.name}
             />
             <div className='roulette'>
-                {pokeball.tiers.flatMap(getByTier).map(pokemon => (
+                {pokemonImg.map(pokemon => (
                     <img src={pokemon.image.sprite} alt={pokemon.name.english} key={pokemon.id + pokemon.name.english}/>
                 ))}
             </div>
