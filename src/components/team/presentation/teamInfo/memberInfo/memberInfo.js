be0317d794de33b645209aefa1dc4fab6f64fb76
Pedro Cisternas <pedrocisternasd@gmail.com>
2024-11-12 11:01:06 -0300

    feat: cry on click

--- src/components/team/presentation/teamInfo/memberInfo/memberInfo.js
+++ src/components/team/presentation/teamInfo/memberInfo/memberInfo.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./memberInfo.styles.scss"
 import Type from "../../../reveal/mobile/type/type";
 import MemberColumn from "./memberColumn/memberColumn";
 import {getPokeballByTier} from "../../../../../utils/utils";
 
-const MemberInfo = ({id, image: {sprite}, name: {english}, type, tier}) => {
+const MemberInfo = ({id, image: {sprite}, name: {english}, type, tier, cry}) => {
+    const [crySfx, setCrySfx] = useState(null);
+    
+    const makeCry = () => {
+        crySfx.currentTime = 0;
+        crySfx.play();
+    };
+    
+    useEffect(() => {
+        setCrySfx(new Audio(cry));
+    }, [cry]);
+    
     return (
-        <div className="memberInfo">
+        <div className="memberInfo" onClick={makeCry}>
             <div className="firstColumn">
                 {<img src={getPokeballByTier(tier).img} alt={getPokeballByTier(tier).name} className="pokeball"/>}
                 <MemberColumn id={id}/>
